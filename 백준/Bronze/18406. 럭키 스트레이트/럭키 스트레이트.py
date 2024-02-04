1e3b6ec325d8bbfbcbe84cfd65b2743544e8fcc5
MinJaeSon <mjsonsj@gmail.com>
2024-02-04 23:36:55 +0900

    [Bronze II] Title: 럭키 스트레이트, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/18406. 럭키 스트레이트/럭키 스트레이트.py
@@ -0,0 +1,14 @@
+N = input()
+left = 0
+right = 0
+
+for i in range(0, len(N)):
+    if i < len(N) / 2:
+        left += int(N[i])
+    else:
+        right += int(N[i])
+
+if left == right:
+    print("LUCKY")
+else:
+    print("READY")
