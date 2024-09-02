fc9b9bae448a02e9df57d483f4f0e59a23fb3fd1
MinJaeSon <mjsonsj@gmail.com>
2024-09-03 00:04:25 +0900

    [Bronze V] Title: A+B, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
 
-const a = Number(input[0]);
-const b = Number(input[1]);
+let [a, b] = input.map(num => Number(num))
 console.log(a + b);
