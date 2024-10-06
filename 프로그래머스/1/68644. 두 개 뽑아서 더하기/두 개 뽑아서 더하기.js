2ca9974a544f0ce33dd8a1ce9266d7c321df5c78
MinJaeSon <mjsonsj@gmail.com>
2024-10-06 13:24:17 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.85 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+    let result = [];
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            result.push(numbers[i] + numbers[j]);
+        }
+    };
+    const set = new Set(result.sort((a, b) => a - b));
+    return [...set];
+}
