ad3a3459a76bcbb2868f1139ac94270d56db921b
MinJaeSon <mjsonsj@gmail.com>
2024-10-09 11:32:40 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 998.31 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,21 +1,3 @@
 function solution(strings, n) {
-    // return strings.sort((a, b) => a[n] > b[n] ? 1 : -1);
-    return strings.sort((a, b) => {
-        console.log()
-        console.log(a, b, '/', a[n], b[n], '/', a[n] > b[n], '/', a > b)
-        
-        if (a[n] !== b[n]) {
-            if (a[n] > b[n]) {
-                return 1
-            } else {
-                return -1
-            }
-        } else {
-            if (a > b) {
-                return 1
-            } else {
-                return -1
-            } 
-        }
-    });
+    return strings.sort((a, b) => a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]));
 }
