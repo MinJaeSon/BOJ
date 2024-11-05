2edd1e0a86b33e89884a6dd4e52552d4ed1a75a0
MinJaeSon <mjsonsj@gmail.com>
2024-11-05 10:45:16 +0900

    [level 1] Title: 폰켓몬, Time: 0.62 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,13 +1,6 @@
 function solution(nums) {
-    let ponketmon = {};
-    nums.forEach((num) => {
-        if (num in ponketmon) {
-            ponketmon[num]++
-        } else {
-            ponketmon[num] = 1
-        }
-    });
-    const kind = Object.keys(ponketmon).length;
+    const ponketmon = new Set(nums);
+    const kind = ponketmon.size;
     const num = nums.length / 2;
     
     return kind < num ? kind : num;
