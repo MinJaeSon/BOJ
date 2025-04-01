b070fdfaa62a8ab6dc3fa10a928e71f840f2268e
MinJaeSon <mjsonsj@gmail.com>
2025-04-01 11:43:40 +0900

    [level 1] Title: 과일 장수, Time: 333.09 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,16 +1,16 @@
 function solution(k, m, score) {
-    var answer = 0;
-    // 1) 사과의 점수를 내림차순으로 정렬한다.
-    score.sort((a, b) => b-a);   
+    // 3322111 -> 3322 -> 8
+    // 444444222211 -> 444 444 222 211 -> 12 12 6 3 = 33
+    let result = 0;
+    const scoreArr = score.sort((a, b) => b - a);
     
-    // 2) 박스에 담을 수 있는 개수(m)을 사용하여 박스의 개수를 구한다.
-    let box = Math.floor(score.length / m);
-
-    // 3) 박스의 개수 만큼 반복문
-    for(let i=1; i<=box; i++) {
-    
-        // 4) (박스에 담긴 최소 값) * m 을 answer에 더한다.
-        answer += score[m*i-1] * m;
+    let startIdx = 0
+    for (let i = 0; i < parseInt(score.length / m); i++) {
+        const box = scoreArr.slice(startIdx, startIdx + m)
+        const min = Math.min(...box)
+        result += (min * m)
+        startIdx += m
     }
-    return answer;
+    
+    return result;
 }
