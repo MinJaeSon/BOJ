8f7c990febcd6b4cd452d50233a4610320d8222b
MinJaeSon <mjsonsj@gmail.com>
2025-04-02 11:42:09 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,7 +1,5 @@
 function solution(s) {    
-    const arr = s.split(' ').map(Number).sort((a, b) => a - b);
-    const min = String(arr[0]);
-    const max = String(arr.reverse()[0]);
-    
-    return min + ' ' + max;
+    const arr = s.split(' ');
+
+    return Math.min(...arr) + ' ' + Math.max(...arr);
 }
