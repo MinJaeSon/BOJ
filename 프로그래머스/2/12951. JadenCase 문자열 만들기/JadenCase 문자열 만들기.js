b220130fbc677c49edbaef4ec53fb16d8189f99a
MinJaeSon <mjsonsj@gmail.com>
2025-04-09 11:51:41 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,3 +1,10 @@
-function solution(s) {
-    return s.toLowerCase().replace(/\b[a-z]/g, (match) => match.toUpperCase());
+function solution(str) {
+    return str.split(' ').map((s) => {
+        if (/^\d/.test(s)) {
+            s = s[0] + s.slice(1).toLowerCase(); // "3People" 같은 경우 고려
+        } else {
+            s = s.slice(0, 1).toUpperCase() + s.slice(1).toLowerCase();
+        }
+        return s;
+    }).join(' ');
 }
