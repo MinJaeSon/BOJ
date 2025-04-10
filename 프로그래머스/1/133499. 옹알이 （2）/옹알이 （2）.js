411e4b282cdd82679327e86e7e464f21fe5a37db
MinJaeSon <mjsonsj@gmail.com>
2025-04-10 14:49:15 +0900

    [level 1] Title: 옹알이 (2), Time: 0.35 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,19 +1,11 @@
 function solution(babbling) {
-    var answer = 0;
-    var arr = ["aya", "ye", "woo", "ma"];
-    
-    for (var i=0; i < babbling.length; i++) {
-        let bab = babbling[i];
-        
-        for (var j=0; j < arr.length; j++) {
-                if(bab.includes(arr[j].repeat(2))) {
-                break;
-                }
-            bab = bab.split(arr[j]).join(" ");
-            }
-        if(bab.split(" ").join("").length === 0){
-            answer ++
+    const regex1 = /(aya|ye|woo|ma)\1+/; // 앞의 단어가 한 번 더 나왔는지 중복 확인
+    const regex2 = /^(aya|ye|woo|ma)+$/; // 전체 문자열이 해당 단어들로만 이루어졌는지 확인
+
+    return babbling.reduce((ans, word) => {
+        if (!regex1.test(word) && regex2.test(word)) {
+            ans++;
         }
-    }
-    return answer;
+        return ans;
+    }, 0)
 }
