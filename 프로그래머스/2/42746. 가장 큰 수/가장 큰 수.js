2f463d8d773189aa77db8211a33f28b182b30e0a
MinJaeSon <mjsonsj@gmail.com>
2025-05-02 11:21:19 +0900

    [level 2] Title: 가장 큰 수, Time: 158.92 ms, Memory: 44.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,6 +1,6 @@
 function solution(numbers) {
     const answer = numbers.map(String)
-                        .sort((a, b) => (b + a) - (a + b))
+                        .sort((a, b) => (b + a)*1 - (a + b)*1)
                         .join('');
     return answer[0] === '0' ? '0' : answer;
 }
