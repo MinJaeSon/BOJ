2a57ec9a03c0b20873b0bc4e77dd11f3fa5e487a
MinJaeSon <mjsonsj@gmail.com>
2025-05-27 11:29:08 +0900

    [Silver IV] Title: ATM, Time: 96 ms, Memory: 9640 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.js
+++ 백준/Silver/11399. ATM/ATM.js
@@ -1,10 +1,10 @@
-const filePath = process.platform == 'linux' ? '/dev/stdin' : './input.txt';
-const [N, input] = require('fs').readFileSync(filePath).toString().trim().split('\n');
-const times = input.split(' ').map(Number).sort((a, b) => a - b);
+const fs = require("fs");
+let [n, arr] = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 let sum = 0;
-let result = 0;
-for (let i = 0; i < N; i++) {
-  sum += times[i];
-  result += sum;
+let answer = 0;
+arr = arr.split(" ").map(Number).sort((a, b) => a - b);
+for (let i = 0; i < n; i++) {
+    sum = sum + arr[i];
+    answer += sum;
 }
-console.log(result);
+console.log(answer);
