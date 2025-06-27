1b1f89d976a857c3dcb1ad4bbb2fd9ebca9e9f53
MinJaeSon <mjsonsj@gmail.com>
2025-06-27 10:42:21 +0900

    [level 1] Title: 달리기 경주, Time: 250.41 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,19 +1,15 @@
 function solution(players, callings) {
-    const playerMap = {};
-  
-    for (let i = 0; i < players.length; i++) {
-        playerMap[players[i]] = i;
-    }
-
-    for (let i = 0; i < callings.length; i++) {
-        const idx = playerMap[callings[i]];
-        const temp = players[idx - 1];
-      
-        players[idx - 1] = callings[i];
-        players[idx] = temp;
-      
-        playerMap[callings[i]] = idx - 1;
-        playerMap[temp] = idx;
+    let rateMap = {};
+    players.forEach((player, i) => rateMap[player] = i);
+    
+    for (const called of callings) {
+        const idx = rateMap[called];
+        const frontPlayer = players[idx - 1];
+        
+        rateMap[called]--;
+        rateMap[frontPlayer]++;
+        
+        [players[idx - 1], players[idx]] = [players[idx], players[idx - 1]];
     }
     
     return players;
