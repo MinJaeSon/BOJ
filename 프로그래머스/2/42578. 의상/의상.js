eacaeb02f586a8dcf74475bb5dfe737975b09016
MinJaeSon <mjsonsj@gmail.com>
2025-06-27 11:30:01 +0900

    [level 2] Title: 의상, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,19 +1,12 @@
 function solution(clothes) {
-    let answer = 1;
-    const obj = {};
-    
-    clothes.map((c) => {
-        if (obj[c[1]]) {
-            return obj[c[1]] += 1;
-        } else {
-            return obj[c[1]] = 1;
-        }
-    });
-
-    const value = Object.values(obj);
-    for(let i = 0; i < value.length; i++) {
-        answer *= value[i] + 1;
+    let obj = {};
+    for (const [val, key] of clothes) {
+        if (!obj[key]) obj[key] = [];
+        obj[key].push(val);
     }
-
+    
+    let answer = 1;
+    Object.keys(obj).forEach((key) => answer *= obj[key].length + 1);
+    
     return answer - 1;
 }
