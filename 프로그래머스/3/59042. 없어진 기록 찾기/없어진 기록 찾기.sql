5bdd02e6fce7b993a56db922192c277d87f780b7
MinJaeSon <mjsonsj@gmail.com>
2025-08-29 00:04:21 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT o.ANIMAL_ID,
+       o.NAME
+FROM ANIMAL_OUTS AS o
+WHERE o.ANIMAL_ID NOT IN (SELECT ANIMAL_ID
+                          FROM ANIMAL_INS)
+ORDER BY o.ANIMAL_ID;
