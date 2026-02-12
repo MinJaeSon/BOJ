ceeab6cfff5442f04e1a3301da2ed161b65737ba
MinJaeSon <mjsonsj@gmail.com>
2026-02-12 15:22:36 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 1.87 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -1,56 +1,55 @@
 function solution(numbers, hand) {
-    const keypad = {
-        '1': '00',
-        '2': '01',
-        '3': '02',
-        '4': '10',
-        '5': '11',
-        '6': '12',
-        '7': '20',
-        '8': '21',
-        '9': '22',
-        '*': '30',
-        '0': '31',
-        '#': '32'
-    };
-    
-    let currentL = '*';
-    let currentR = '#';
-    let result = [];
-    
-    for (let num of numbers) {
-        if ([1, 4, 7].some((n) => n === num)) {
-            result.push('L');
-            currentL = num;
-        } else if ([3, 6, 9].some((n) => n === num)) {
-            result.push('R');
-            currentR = num;
-        } else {
-            const distanceL = getDistance(currentL, String(num));
-            const distanceR = getDistance(currentR, String(num));
-            if (distanceL > distanceR) {
-                result.push('R');
-                currentR = num;
-            } else if (distanceL < distanceR) {
-                result.push('L');
-                currentL = num;
-            } else {
-                result.push(hand === 'right' ? 'R' : 'L');
-                if (hand === 'right') currentR = num;
-                else currentL = num;
+    const numpad = [[1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9],
+                    ['*', 0,'#']];
+    let result = '';
+    let [leftX, leftY] = [3, 0];
+    let [rightX, rightY] = [3, 2];
+    let [targetX, targetY] = [0, 0];
+
+    for (const num of numbers) {
+        if ([1, 4, 7].includes(num)) {
+            for (let row = 0; row < 4; row++) {
+                if (numpad[row][0] === num) {
+                    [leftX, leftY] = [row, 0];
+                }
             }
-        }
+            result += 'L';
+        } else if ([3, 6, 9].includes(num)) {
+            for (let row = 0; row < 4; row++) {
+                if (numpad[row][2] === num) {
+                    [rightX, rightY] = [row, 2];
+                }
+            }
+            result += 'R';
+        } else {
+            for (let row = 0; row < 4; row++) {
+                if (numpad[row][1] === num) {
+                    [targetX, targetY] = [row, 1];
+                }
+            }
+
+            const leftD = Math.abs(leftX - targetX) + Math.abs(leftY - targetY);
+            const rightD = Math.abs(rightX - targetX) + Math.abs(rightY - targetY);
+            
+            if (leftD < rightD) {
+                [leftX, leftY] = [targetX, targetY];
+                result += 'L';
+            } else if (leftD > rightD) {
+                [rightX, rightY] = [targetX, targetY];
+                result += 'R';
+            } else {
+                if (hand === 'right') {
+                    [rightX, rightY] = [targetX, targetY];
+                    result += 'R';
+                } else {
+                    [leftX, leftY] = [targetX, targetY];
+                    result += 'L';
+                }
+            }
+        } 
     }
     
-    return result.join('');
-    
-    function getDistance(current, target) {
-        let distance = 0;
-        
-        for (let i = 0; i < 2; i++) {
-            distance += Math.abs(keypad[current][i] - keypad[target][i]);
-        }
-        
-        return distance;
-    }
+     return result;
 }
