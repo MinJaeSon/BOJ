c1c11f758a79382b1f1bc5b82bc15cab26827d40
MinJaeSon <mjsonsj@gmail.com>
2026-02-15 18:11:11 +0900

    [level 2] Title: 기능개발, Time: 4.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,22 +1,25 @@
 function solution(progresses, speeds) {
     let result = [];
-    let stack = [];
+    const rests = progresses.map((progress) => 100 - progress);
+    const costs = rests.map((rest, i) => Math.ceil(rest / speeds[i]));
     
-    progresses.forEach((progress, i) => {
-        const cost = Math.ceil((100 - progress) / speeds[i]);
-        if (stack.length) {
-            if (stack[stack.length - 1] >= cost) {
-                stack.push(stack[stack.length - 1]);
-            } else {
-                result.push(stack.length);
-                stack = [cost];
-            }
-        } else {
-            stack.push(cost);
+    let count = 0;
+    let standard = costs[0];
+    
+    for (let i = 0; i < costs.length; i++) {
+        console.log(i);
+        if (count === 0) {
+            standard = costs[i];
         }
-    })
-    
-    result.push(stack.length);
+        count++;
+        
+        if (standard < costs[i + 1] || i === costs.length - 1) {
+            result.push(count);
+            count = 0;
+        }
+    }
+        // [5, 10, 1, 1, 20, 1]
+        // 5 (1) / 10, 1, 1 (3) / 20, 1 (2)
     
     return result;
 }
