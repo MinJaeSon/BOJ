6e9924848c396e9f8ba3829bd10b6fcd76b6dbf3
MinJaeSon <mjsonsj@gmail.com>
2026-02-19 11:11:56 +0900

    [level 2] Title: 괄호 회전하기, Time: 64.71 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,52 +1,49 @@
 function solution(s) {
-    let answer = 0;
-    s = [...s]
-    
-    for (let x = 0; x < s.length; x++) {
-        if (x > 0) s.push(s.shift());
-        
-        if (isProper(s)) {
-            answer++;
-        }
-    }
-    
-    return answer;
-}
-
-function isProper(s) {
-    let stack = [];
+    let count = 0;
+    let queue = [...s];
     
     for (let i = 0; i < s.length; i++) {
-        if (!stack.length) {
-            stack.push(s[i]);
-        } else {
-            switch (s[i]) {
-                case ']':
-                    if (stack[stack.length - 1] === '[') {
-                        stack.pop();
-                    } else {
-                        stack.push(s[i]);
-                    }
-                    break;
-                case '}':
-                    if (stack[stack.length - 1] === '{') {
-                        stack.pop();
-                    } else {
-                        stack.push(s[i]);
-                    }
-                    break;
-                case ')':
-                    if (stack[stack.length - 1] === '(') {
-                        stack.pop();
-                    } else {
-                        stack.push(s[i]);
-                    }
-                    break;
-                default:
-                    stack.push(s[i]);
+        let stack = [];
+        
+        for (let j = 0; j < queue.length; j++) {
+            const cur = queue[j];
+            
+            if (stack.length) {
+                const top = stack[stack.length - 1];
+                switch (cur) {
+                    case ']':
+                        if (top === '[') {
+                            stack.pop();
+                        } else {
+                            stack.push(cur);
+                        }
+                        break;
+                    case ')':
+                        if (top === '(') {
+                            stack.pop();
+                        } else {
+                            stack.push(cur);
+                        }
+                        break;
+                    case '}':
+                        if (top === '{') {
+                            stack.pop();
+                        } else {
+                            stack.push(cur);
+                        }
+                        break;
+                    default:
+                        stack.push(cur);
+                }
+            } else {
+                stack.push(cur);
             }
         }
+        
+        if (!stack.length) count++;
+        
+        queue.push(queue.shift());
     }
     
-    return stack.length ? false : true;
+    return count;
 }
