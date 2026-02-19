b199a32f25f4f0680d80ce5a66a956f1387b8ee8
MinJaeSon <mjsonsj@gmail.com>
2026-02-19 11:35:01 +0900

    [level 2] Title: 카펫, Time: 32.49 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -4,11 +4,9 @@ function solution(brown, yellow) {
     for (let a = sum; a > 0; a--) {
         const b = sum / a;
         if (!Number.isInteger(b)) continue;
-        if (a < b) break;
         
-        if ((a + b - 2) * 2 === brown && (a - 2) * (b - 2) === yellow) {
+        if ((a - 2) * (b - 2) === yellow) {
             return [a, b];
         }
     }
-
 }
