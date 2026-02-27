a058a4a3c5c0fe62e2b8aecedfa6afaecdfc3d5a
MinJaeSon <mjsonsj@gmail.com>
2026-02-27 10:02:20 +0900

    [level 2] Title: 타겟 넘버, Time: 14.54 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,18 +1,21 @@
 function solution(numbers, target) {
-    var answer = 0;
+    // 각 숫자마다 선택지가 두 개(+, -) -> 이진 트리 구조 -> 끝까지 가야 합이 완성됨 -> DFS
+    let count = 0;
     
-    dfs(0,0);
+    dfs(0, 0);
     
-    return answer;
+    return count;
     
-    function dfs(index, sum) {
-        if (index === numbers.length) {
-            if (sum === target) {
-                answer++;
+    function dfs(index, total) { // index = depth
+        if (index === numbers.length) { // 재귀 종료 조건
+            if (total === target) {
+                count++;
             }
             return;
         }
-        dfs(index + 1, sum + numbers[index]);
-        dfs(index + 1, sum - numbers[index]);
+        
+        dfs(index + 1, total + numbers[index]); // 해당 depth에서 + 선택 시 뻗는 갈래
+        dfs(index + 1, total - numbers[index]); // 해당 depth에서 - 선택 시 뻗는 갈래
     }
+            
 }
