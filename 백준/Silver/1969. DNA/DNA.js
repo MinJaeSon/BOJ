8670d607a6630b53e7ceed8ad3fba63230d305aa
MinJaeSon <mjsonsj@gmail.com>
2026-03-03 12:33:35 +0900

    [Silver IV] Title: DNA, Time: 160 ms, Memory: 11028 KB -BaekjoonHub

--- 백준/Silver/1969. DNA/DNA.js
+++ 백준/Silver/1969. DNA/DNA.js
@@ -1,26 +1,21 @@
 const fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-const [N, M] = input[0].split(' ').map(Number);
-const dnas = input.slice(1);
-let result = "";
+let [input, ...arr] = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+let [N, M] = input.split(' ').map(Number);
+let mode = '';
+let s = '';
 let hd = 0;
 
 for (let i = 0; i < M; i++) {
     let nucleotides = new Map();
 
-    for (let dna of dnas) {
-        const nucleotide = dna[i];
-        if (nucleotides.has(nucleotide)) {
-            nucleotides.set(nucleotide, nucleotides.get(nucleotide) + 1);
-        } else {
-            nucleotides.set(nucleotide, 1);
-        }
+    for (let j = 0; j < N; j++) {
+        const nucleotide = arr[j][i];
+        nucleotides.set(nucleotide, (nucleotides.get(nucleotide) || 0) + 1);
     }
 
-    const mode = [...nucleotides].sort((a, b) => a[0].localeCompare(b[0])) // -1 (compareFn: a - b 인거랑 같은 셈. 문자열 비교니까 localCompare 사용)
-                                .reduce((acc, cur) => acc[1] >= cur[1] ? acc : cur)[0];
-    result += mode;
-
+    mode = [...nucleotides].sort((a, b) => b[1] - a[1] || a[0].localeCompare(b[0]))[0][0];
+    s += mode;
+    
     hd = [...nucleotides].reduce((acc, cur) => {
         if (cur[0] !== mode) {
             acc += cur[1];
@@ -29,5 +24,5 @@ for (let i = 0; i < M; i++) {
     }, hd);
 }
 
-console.log(result);
+console.log(s);
 console.log(hd);
