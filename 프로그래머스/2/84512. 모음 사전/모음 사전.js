87c8d3766a27922be59561af283dce3dbd4bae3f
MinJaeSon <mjsonsj@gmail.com>
2026-03-05 13:23:51 +0900

    [level 2] Title: 모음 사전, Time: 1.06 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,25 +1,20 @@
 function solution(word) {
     const vowels = ['A', 'E', 'I', 'O', 'U'];
-    let answer = 0;
-    let count = 0;
+    const list = [];
     
     function dfs(cur) {
-        if (cur === word) {
-            answer = count;
-            return true; // 단어를 찾았으니 dfs 탐색을 멈추도록 하기 위해서 true 값을 반환
+        if (cur.length > 0) {
+            list.push(cur);
         }
         
-        if (cur.length === 5) return false; // 여기는 없으니까 다른 경로 탐색하도록 넘김
+        if (cur.length === 5) return;
         
         for (let i = 0; i < 5; i++) {
-            count++;
-            if (dfs(cur + vowels[i])) return true; // true 받고 나도 true 반환
+            dfs(cur + vowels[i]);
         }
-        
-        return false;
     }
     
     dfs('');
     
-    return answer;
+    return list.indexOf(word) + 1;
 }
