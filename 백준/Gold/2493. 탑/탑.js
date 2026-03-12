5c1a27752cea9ef3c3e757f92257c8f1bc994ee8
MinJaeSon <mjsonsj@gmail.com>
2026-03-12 10:10:05 +0900

    [Gold V] Title: 탑, Time: 436 ms, Memory: 125804 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.js
+++ 백준/Gold/2493. 탑/탑.js
@@ -1,20 +1,20 @@
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = require('fs').readFileSync(filePath).toString().split('\n');
+const fs = require("fs");
+let [N, tops] = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+N = Number(N);
+tops = tops.split(' ').map(Number);
 
-const n = Number(input[0]);
-const arr = input[1].split(' ').map(Number);
+let stack = [];
+let answer = [];
 
-const stack = [];
-const ans = [];
+for (let i = 0; i < N; i++) {
 
-for (let i = 0; i < n; i += 1) {
-  while (stack.length > 0 && arr[stack.at(-1)] < arr[i]) {
-    stack.pop();
-  }
+    while (stack.length && tops[stack[stack.length - 1]] < tops[i]) { // 스택의 탑에 있는게 현재 탑보다 작으면 스택 안이 내림차순이 안되기 떄문에 내림차순으로 만들어줌
+        stack.pop();
+    }
 
-  ans.push((stack.at(-1) ?? -1) + 1);
+    answer.push(stack.length ? stack[stack.length - 1] + 1 : 0); // 스택이 비었으면 0(자기보다 큰 애가 없는 거니까 레이저를 맞을 애가 없음), 그렇지 않으면 그 탑의 순서(그 탑이 왼쪽에서 가장 먼저 레이저를 맞는 탑)
 
-  stack.push(i);
+    stack.push(i);
 }
 
-console.log(ans.join(' '));
+console.log(answer.join(' '));
