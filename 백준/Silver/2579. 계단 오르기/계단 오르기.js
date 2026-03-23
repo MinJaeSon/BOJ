e8ba6967aacc3c5c3db1823b186feee71747ba15
MinJaeSon <mjsonsj@gmail.com>
2026-03-23 15:21:54 +0900

    [Silver III] Title: 계단 오르기, Time: 88 ms, Memory: 9368 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.js
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
-let [n, ...input] = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map(Number);
-const d = Array(n).fill(0);
+let [N, ...arr] = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map(Number);
+arr = [0, ...arr];
 
-d[0] = input[0];
-d[1] = input[0] + input[1];
-d[2] = Math.max(input[0] + input[2], input[1] + input[2]);
+const dp = Array(N + 1).fill(0);
+dp[1] = arr[1]; 
+dp[2] = dp[1] + arr[2];
 
-for (let i = 3; i < n; i++) {
-    d[i] = Math.max(d[i - 3] + input[i - 1], d[i - 2]) + input[i];
+for (let i = 3; i <= N; i++) {
+    dp[i] = Math.max(dp[i - 3] + arr[i - 1], dp[i - 2]) + arr[i];
 }
 
-console.log(d[n - 1]);
+console.log(dp[N]);
