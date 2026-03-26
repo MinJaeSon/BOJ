a53edf76df91069e2a19a4cd56e79d5e17c16ab9
MinJaeSon <mjsonsj@gmail.com>
2026-03-26 12:35:26 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 188 ms, Memory: 11964 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
@@ -1,57 +1,33 @@
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-const N = Number(input[0]);
-const arr = input[1].split(" ").map((n) => BigInt(n));
-const op_nums = input[2].split(" ").map(Number); // [+, -, x, ÷]
-let max = null;
-let min = null;
-const temp = [];
+const stdin = require('fs').readFileSync('/dev/stdin').toString().trim();
+const input = stdin.split('\n').map(v => v.split(' ').map(Number));
+const [N, A, operators] = input;
 
-function dfs(depth, operators) {
-  if (depth === N - 1) {
-    calculate(operators);
-    return;
-  }
+const calculate = [
+  (a, b) => a + b,
+  (a, b) => a - b,
+  (a, b) => a * b,
+  (a, b) => ~~(a / b)
+];
 
-  for (let i = 0; i < op_nums.length; i++) {
-    if (op_nums[i] === 0) {
-      continue;
-    }
+let max = -1_000_000_000;
+let min = 1_000_000_000;
 
-    op_nums[i] -= 1;
-    temp.push(i); // + 이면 0, - 이면 1, ...
-    dfs(depth + 1, temp);
-    temp.pop();
-    op_nums[i] += 1;
-  }
-}
-
-function calculate(operators) {
-  let total = BigInt(arr[0]);
-
-  for (let i = 1; i <= N - 1; i++) {
-    const op = operators[i - 1];
-    switch (op) {
-      case 0:
-        total += arr[i];
-        break;
-      case 1:
-        total -= arr[i];
-        break;
-      case 2:
-        total *= arr[i];
-        break;
-      case 3:
-        total = total / arr[i];
-        break;
+const dfs = (count = 0, result = A[0]) => {
+  if (count === N - 1) {
+    max = Math.max(max, result);
+    min = Math.min(min, result);
+  } else {
+    for (let i = 0; i < 4 ; i++) {
+      if (!operators[i]) {
+        continue;
+      }
+      operators[i]--;
+      dfs(count + 1, calculate[i](result, A[count + 1]));
+      operators[i]++;
     }
   }
+};
 
-  if (max === null || total > max) max = total;
-  if (min === null || total < min) min = total;
-}
-
-dfs(0);
-
-console.log(Number(max));
-console.log(Number(min));
+dfs();
+console.log(max);
+console.log(min);
