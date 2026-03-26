80a4d49f2ca9e913ac055ed5b918c5f099001269
MinJaeSon <mjsonsj@gmail.com>
2026-03-26 13:41:03 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 168 ms, Memory: 10936 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
@@ -1,33 +1,57 @@
-const stdin = require('fs').readFileSync('/dev/stdin').toString().trim();
-const input = stdin.split('\n').map(v => v.split(' ').map(Number));
-const [N, A, operators] = input;
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const N = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
+const op_nums = input[2].split(' ').map(Number); // [+, -, x, ÷]
+let max = -Infinity;
+let min = Infinity;
+const temp = [];
 
-const calculate = [
-  (a, b) => a + b,
-  (a, b) => a - b,
-  (a, b) => a * b,
-  (a, b) => ~~(a / b)
-];
+function dfs(depth) {
+  if (depth === N - 1) {
+    calculate(temp);
+    return;
+  }
 
-let max = -1_000_000_000;
-let min = 1_000_000_000;
+  for (let i = 0; i < op_nums.length; i++) {
+    if (op_nums[i] === 0) {
+      continue;
+    }
 
-const dfs = (count = 0, result = A[0]) => {
-  if (count === N - 1) {
-    max = Math.max(max, result);
-    min = Math.min(min, result);
-  } else {
-    for (let i = 0; i < 4 ; i++) {
-      if (!operators[i]) {
-        continue;
-      }
-      operators[i]--;
-      dfs(count + 1, calculate[i](result, A[count + 1]));
-      operators[i]++;
+    op_nums[i] -= 1;
+    temp.push(i); // + 이면 0, - 이면 1, ...
+    dfs(depth + 1);
+    temp.pop();
+    op_nums[i] += 1;
+  }
+}
+
+function calculate(operators) {
+  let total = arr[0];
+
+  for (let i = 1; i <= N - 1; i++) {
+    const op = operators[i - 1];
+    switch(op) {
+      case 0:
+        total += arr[i];
+        break;
+      case 1:
+        total -= arr[i];
+        break;
+      case 2:
+        total *= arr[i];
+        break;
+      case 3:
+        total = Math.trunc(total / arr[i]) || 0; // js에서 0과 -0은 다른 값이므로, -0을 0으로 변환 필요
+        break;
     }
   }
-};
 
-dfs();
+  max = Math.max(max, total);
+  min = Math.min(min, total);
+}
+
+dfs(0);
+
 console.log(max);
 console.log(min);
