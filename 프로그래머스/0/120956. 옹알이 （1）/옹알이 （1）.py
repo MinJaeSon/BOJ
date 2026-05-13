99d1db4a1d920ca13600202c6088ab6748f60c47
MinJaeSon <mjsonsj@gmail.com>
2026-05-14 01:04:44 +0900

    [level 0] Title: 옹알이 (1), Time: 0.05 ms, Memory: 9.33 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -0,0 +1,16 @@
+def solution(babbling):
+    available = ["aya", "ye", "woo", "ma"]
+    
+    answer = 0
+    
+    for i in babbling:
+        result = 0
+        
+        for j in range(4):
+            if i.find(available[j]) != -1:
+                result += len(available[j])
+                
+        if len(i) == result:
+            answer += 1
+        
+    return answer
